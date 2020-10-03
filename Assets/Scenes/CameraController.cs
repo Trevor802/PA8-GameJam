baee8faf8092cab8aa9cb26fe7a9da11066e297b
Trevor Alex <pascerveau@gmail.com>
2020-10-03 23:58:13 +0800

    [movement] setup on turn event

---
+++ Assets/Scenes/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private InterruptibleCoroutine m_camRotCoroutine = default;
+	private float m_rotationSec = 0.5f;
+    void Start()
+    {
+        var player = GetComponentInParent<PlayerController>();
+        player.OnTurnLeft.AddListener(OnTurnLeft);
+        player.OnTurnRight.AddListener(OnTurnRight);
+    }
+
+    private void OnTurnLeft(){
+        var prevTargetRot = m_camRotCoroutine.StopCoroutine(this, false);
+		var targetRot = prevTargetRot is null ? Quaternion.LookRotation(-transform.right) :
+			(Quaternion)prevTargetRot * Quaternion.Euler(0, -90f, 0);
+		InterruptibleCoroutine.Callback afterRot = (bool execute) => {
+			if (execute){
+				transform.rotation = targetRot;
+			}
+			return targetRot;
+		};
+		m_camRotCoroutine = StartCoroutine(gameObject.Turn(targetRot, afterRot, m_rotationSec)).WrapAction(afterRot);
+
+    }
+
+	private void OnTurnRight(){
+		var prevTargetRot = m_camRotCoroutine.StopCoroutine(this, false);
+		var targetRot = prevTargetRot is null ? Quaternion.LookRotation(transform.right) :
+			(Quaternion)prevTargetRot * Quaternion.Euler(0, 90f, 0);
+		InterruptibleCoroutine.Callback afterRot = (bool execute) => {
+			if (execute){
+				transform.rotation = targetRot;
+			}
+			return targetRot;
+		};
+		m_camRotCoroutine = StartCoroutine(gameObject.Turn(targetRot, afterRot, m_rotationSec)).WrapAction(afterRot);
+	}
+}
--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -1,46 +1,36 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Vector3 m_movingDir = Vector3.forward;
-	private Camera m_camera = default;
-	private InterruptibleCoroutine m_camRotCoroutine = default;
-	private float m_rotationSec = 0.5f;
+	private CameraController m_camera = default;
 	private float m_movingSpeed = 10f;
-    void Start()
+	public UnityEvent OnTurnLeft;
+	public UnityEvent OnTurnRight;
+    private void Awake()
     {
-		m_camera = GetComponentInChildren<Camera>();
+		if (OnTurnLeft is null){
+			OnTurnLeft = new UnityEvent();
+		}
+		if (OnTurnRight is null){
+			OnTurnRight = new UnityEvent();
+		}
+		OnTurnLeft.RemoveAllListeners();
+		OnTurnRight.RemoveAllListeners();
     }
 
     private void TurnLeft(){
 		m_movingDir = m_movingDir.TurnLeft();
-		var prevTargetRot = m_camRotCoroutine.StopCoroutine(this, false);
-		var targetRot = prevTargetRot is null ? Quaternion.LookRotation(-m_camera.transform.right) :
-			(Quaternion)prevTargetRot * Quaternion.Euler(0, -90f, 0);
-		InterruptibleCoroutine.Callback afterRot = (bool execute) => {
-			if (execute){
-				m_camera.transform.rotation = targetRot;
-			}
-			return targetRot;
-		};
-		m_camRotCoroutine = StartCoroutine(m_camera.gameObject.Turn(targetRot, afterRot, m_rotationSec)).WrapAction(afterRot);
+		OnTurnLeft.Invoke();
     }
 
     private void TurnRight(){
 		m_movingDir = m_movingDir.TurnRight();
-		var prevTargetRot = m_camRotCoroutine.StopCoroutine(this, false);
-		var targetRot = prevTargetRot is null ? Quaternion.LookRotation(m_camera.transform.right) :
-			(Quaternion)prevTargetRot * Quaternion.Euler(0, 90f, 0);
-		InterruptibleCoroutine.Callback afterRot = (bool execute) => {
-			if (execute){
-				m_camera.transform.rotation = targetRot;
-			}
-			return targetRot;
-		};
-		m_camRotCoroutine = StartCoroutine(m_camera.gameObject.Turn(targetRot, afterRot, m_rotationSec)).WrapAction(afterRot);
+		OnTurnRight.Invoke();
     }
 
     void Update()
