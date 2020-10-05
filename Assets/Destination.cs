f3ff53f5f91b34b162ce8770a251f28336749ecc
Xiubo Zhao <906672365@qq.com>
2020-10-05 19:18:04 +0800

    WinZone

---
+++ Assets/Destination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (other.gameObject.transform.right == transform.right)
+                PlayerWin();
+                //
+        }
+    }
+
+    private void PlayerWin()
+    {
+        Debug.Log("WINN");
+        // TODO: Link the other levels here
+    }
+}
