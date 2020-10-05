6c21dde5f5f0e2d13cfa1b0c79f19ed06513f273
Xiubo Zhao <906672365@qq.com>
2020-10-05 23:34:16 +0800

    Add HUD to level and winning text

--- Assets/Destination.cs
+++ Assets/Destination.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Destination : MonoBehaviour
 {
+    public Text winText;
     private void Start()
     {
         GetComponent<MeshRenderer>().enabled = false;
+
+        if (winText == null)
+            throw new System.Exception("Winning Text is not set");
     }
 
 
@@ -22,7 +27,6 @@ public class Destination : MonoBehaviour
 
     private void PlayerWin()
     {
-        Debug.Log("WINN");
-        // TODO: Link the other levels here
+        winText.enabled = true;
     }
 }
